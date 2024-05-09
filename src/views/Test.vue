7a86782e0fa2987dd07d6cce8fd4c9fbf030aa10
ohyabe <a2591212948@gmail.com>
2024-05-09 16:26:14 +0800

    add

---
+++ src/views/Test.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+
+<div class="btn-group">
+  <a href="#" class="btn btn-primary active" aria-current="page">Active link</a>
+  <a href="#" class="btn btn-primary">Link</a>
+  <a href="#" class="btn btn-primary">Link</a>
+
+  <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+  <button type="button" class="btn btn-danger">Left</button>
+  <button type="button" class="btn btn-warning">Middle</button>
+  <button type="button" class="btn btn-success">Right</button>
+</div>
+</div>
+
+</template>
+
+<style>
+</style>
