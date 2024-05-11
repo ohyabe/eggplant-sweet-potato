9476435f7180438e0c8015201ef0eec1882d0a00
ohyabe <a2591212948@gmail.com>
2024-05-11 21:54:32 +0800

    add mis

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const router = createRouter({
       component: () => import('../views/fetch.vue')
     },
     {
-<<<<<<< HEAD
+
       path: '/fetch2',
       name: 'fetch2',
       component: () => import('../views/fetch2.vue')
@@ -29,12 +29,12 @@ const router = createRouter({
       path: '/fetch3',
       name: 'fetch3',
       component: () => import('../views/fetch3.vue')
-=======
-      path: '/tainanMap',
-      name: 'tainanMap',
-      component: () => import('../views/tainanMap.vue')
->>>>>>> c4931c6d80b1e7022c8e9aa76bf25b3c0c7c42ff
-    }
+    },
+    //   path: '/tainanMap',
+    //   name: 'tainanMap',
+    //   component: () => import('../views/tainanMap.vue')
+
+    // }
   ]
 })
 
