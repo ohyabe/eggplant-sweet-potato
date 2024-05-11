c4931c6d80b1e7022c8e9aa76bf25b3c0c7c42ff
debby031025 <debby03102562@gmail.com>
2024-05-11 17:47:44 +0800

    add import Header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/Test">Test</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <div class="header">
+  <Header />
+</div>
   <RouterView />
 </template>
 
